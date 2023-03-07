42327890426a002338b64919658aaf65cca1bf80
caro barreiro <42847079+carobarreirov@users.noreply.github.com>
2023-03-07 12:31:00 -0600

    changing announcements titles

--- assets/data/courses_announcements.js
+++ assets/data/courses_announcements.js
@@ -1,18 +1,18 @@
 var data = [
     {
-        "title":"New Bootcampt",
+        "title":"Design Thinking",
         "description":"Bootcamp is a 3 day event. A coding bootcamp is a concentrated course that teaches essential skills and provides real-world training for people seeking jobs in computation.",
         "url":"#announcement1",
         "id":"announcement1"
     },
     {
-        "title":"New City!",
+        "title":"Desarrollo Web y Pensamiento Computacional",
         "description":"The City of Boston will be hosting a bootcamp in the summer of 2024.",
         "url":"#announcement2",
         "id":"announcement2"        
     },
     {
-        "title":"Python Track",
+        "title":"Análisis de Datos",
         "description":"You can now choose a python track bootcamp.",
         "url":"#announcement3",
         "id":"announcement3"        
